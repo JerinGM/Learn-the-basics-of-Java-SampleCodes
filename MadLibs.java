c45a2a2c2ef8b9c85342ca1471514ac99b07731a
Jerin Mathew <127280408+JerinGM@users.noreply.github.com>
2023-08-28 11:19:52 +0530

    Sample Java code - Mad libs

---
+++ MadLibs.java
@@ -0,0 +1,18 @@
+public class MadLibs
+{
+    public static void main(String[] args)
+    {
+        String story;
+        String name1 = "John";
+        String noun1 = "laptop";
+        String noun2 = "cup";
+        String noun3 = "watch";
+        String noun4 = "mobile";
+        String adjective1 = "red";
+        String adjective2 = "green";
+        String adjective3 = "blue";
+        String verb1 = "running";
+        story = "This morning "+name1+" woke up feeling "+adjective1+" . 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". This exercise ends here";
+        System.out.println(story);
+    }
+}
