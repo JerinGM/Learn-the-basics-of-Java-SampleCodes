41c03a4c6ca86099cc894ae340dce421fd510e5a
Jerin Mathew <127280408+JerinGM@users.noreply.github.com>
2023-08-28 10:50:13 +0530

    Add files via upload
    Simple Codes for beginners: Planting Tree - Using spaces and '*'

---
+++ plantingtree.java
@@ -0,0 +1,14 @@
+//Writing a simple Code, using spaces and * to plant a tree
+public class plantingtree {
+    public static void main(String[] args)
+    {
+        System.out.println("     *     ");
+        System.out.println("    ***    ");
+        System.out.println("   *****   ");
+        System.out.println("  *******  ");
+        System.out.println(" ********* ");
+        System.out.println("***********");
+        System.out.println("     *     ");
+        System.out.println("     *     ");
+    }
+}
