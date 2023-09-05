13e632262ed00c8db696deb3177d5ed9a23c0090
Jerin Mathew <127280408+JerinGM@users.noreply.github.com>
2023-09-05 13:31:11 +0530

    Delete MathMagic.java

--- MathMagic.java
+++
@@ -1,9 +0,0 @@
-public class MathMagic
-{
-    public static void main(String[] args)
-    {
-        int newnumber = 10;
-        int magicnumber = ((((((newnumber * newnumber) + newnumber) / newnumber) + 20) - newnumber)/5);
-        System.out.println(magicnumber);
-    }
-}
